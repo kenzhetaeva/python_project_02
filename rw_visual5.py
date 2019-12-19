9e2242c21a5aa956cf3400fa06fed8f1edd7f874
kenzhetaeva <kenzhetaeva11@gmail.com>
2019-12-19 12:12:21 +0600

    Создание класса RandomWalk(), Выбор направления, Вывод случайного блуждания, Генерирование нескольких случайных блужданий, Назначение цветов, Начальные и конечные точки, Удаление осей

---
+++ rw_visual5.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Построение случайного блуждания и нанесение точек на диаграмму.
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)    
+    # Выделение первой и последней точек.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',s=100)
+    
+    # Удаление осей.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
